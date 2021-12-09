0ff844815599e69ea11fb15cda2c5719a2a61743
Giuseppe Mascellaro <giuseppe.mascellaro@mail.polimi.it>
2021-12-09 19:17:47 +0100

    add: first bunch of files

---
+++ setup.py
@@ -0,0 +1,54 @@
+from configparser import ConfigParser
+
+from setuptools import find_packages, setup
+
+
+config = ConfigParser()
+config.read('setup.cfg')
+
+setup(
+    name=config.get('project', 'name'),
+    version=config.get('project', 'version'),
+    description=config.get('project', 'description'),
+    author='Giuseppe Mascellaro',
+    author_email='giuseppe.mascellaro@mail.polimi.it',
+    license='Apache License 2.0',
+    url=config.get('github', 'repository_url'),
+    packages=find_packages(where='src', exclude=('tests.*', 'tests')),
+    package_dir={'': 'src'},
+    zip_safe=False,
+    install_requires=[
+        'python-decouple==3.5',
+        'fastapi==0.70.0',
+        'uvicorn==0.16.0',
+        'numpy==1.21.4',
+        'pandas==1.3.4',
+        'scipy==1.7.3',
+    ],
+    extras_require={
+        'dev': [
+            'ipython==7.18.1',
+        ],
+        'lint': [
+            'flake8-builtins<2.0.0',
+            'flake8-commas<3.0.0',
+            'flake8-comprehensions<2.0.0',
+            'flake8-debugger<4.0.0',
+            'flake8-import-order<1.0.0',
+            'flake8-mypy<18.0.0',
+            'flake8-quotes<1.0.0',
+            'flake8-todo<1.0',
+            'flake8<4.0.0',
+            'pep8-naming<1.0.0',
+        ],
+        'test': [
+            'pytest<4.0.0',
+            'pytest-cov<3.0.0',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'assist = typing-assistant.assist:main',
+        ],
+    },
+)
