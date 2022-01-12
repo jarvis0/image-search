4f36c065d41be6af63da39649a3324b90ebece0f
Giuseppe Mascellaro <giuseppe.mascellaro@mail.polimi.it>
2022-01-13 00:31:43 +0100

    add: images drawing (#7)

--- src/typing_assistant/ranking/ranker.py
+++ src/typing_assistant/ranking/ranker.py
@@ -8,6 +8,8 @@ from ..indexing import Collection, Lexicon
 
 class OkapiBM25Ranker:
 
+    N_RESULTS: int = 5
+
     def __init__(self, collection: Collection, lexicon: Lexicon, kappa: float = 1.5, beta: float = 0.75):
         self.collection: Collection = collection
         self.lexicon: Lexicon = lexicon
@@ -33,5 +35,5 @@ class OkapiBM25Ranker:
                 scores[doc_id] += weight * idf[w] * ((self.kappa + 1) * tf[w][doc_id] / (
                     tf[w][doc_id] + self.kappa * (
                         1 - self.beta + self.beta * self.collection.get_doc_length(doc_id) / self.avgdl)))
-        sorted_results = list(sorted(scores.items(), key=lambda x: x[1], reverse=True))[: 10]
-        return [(self.collection.get_document(doc_id), score) for doc_id, score in sorted_results]
+        sorted_results = list(sorted(scores.items(), key=lambda x: x[1], reverse=True))[: OkapiBM25Ranker.N_RESULTS]
+        return [(doc_id, self.collection.get_document(doc_id), score) for doc_id, score in sorted_results]
