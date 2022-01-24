913a76b38f22fe377eca8dbde2ffe2ce46a04943
Giuseppe Mascellaro <giuseppe.mascellaro@mail.polimi.it>
2022-01-25 00:59:34 +0100

    add: working incremental search

--- server/flask_server.py
+++ server/flask_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, render_template, request
+from flask import Flask, escape, jsonify, render_template, request
 
 from typing_assistant.binaries_loader import load_binaries
 from typing_assistant.ranking import OkapiBM25Ranker
@@ -11,13 +11,21 @@ app = Flask(__name__, template_folder='../server/templates')
 
 @app.route('/')
 def index():
-    partial_query = str(escape(request.form.get('input_query', '')))
-    return render_template('index.html', partial_query=partial_query)
+    return render_template('index.html')
+
+
+@app.route('/partial_query', methods=['POST'])
+def partial_query():
+    partial_query = str(escape(request.get_json()['partial_query']))
+    query_results = ranker.lookup_query(partial_query)
+    query_results = [text.text for _, text, _ in query_results]
+    response = {'query_results': query_results}
+    return jsonify(response)
 
 
 @app.route('/query', methods=['POST'])
 def query():
-    query = str(escape(request.form.get('input_query', '')))
+    query = str(escape(request.form.get('input_search', '')))
     query_results = ranker.lookup_query(query)
     query_results = [(images_handler.get_url(doc_id), text, round(score, 4)) for doc_id, text, score in query_results]
     return render_template('query.html', query=query, query_results=query_results)
